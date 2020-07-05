61f40543860e07bc8534a68f9bfc8dee54567381
Pedro <pedro-gomes17@hotmail.com>
2020-07-05 17:47:07 -0300

    Componente de modal para edição dos dados

---
+++ src/components/Modal/index.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import { toast } from 'react-toastify';
+import { Container, Card, Icon } from './styles';
+
+export default function Modal({ onClose, open, data }) {
+  const history = useHistory();
+
+  const [informations, setInformations] = useState([]);
+
+  const { date_created, title, description } = data;
+
+  const initialValues = {
+    date_created,
+    title,
+    description,
+  };
+
+  useEffect(() => {
+    const info = localStorage.getItem('@NASA');
+    if (info) {
+      setInformations(JSON.parse(info));
+    }
+  }, []);
+
+  function handleEdit({ nasa_id, date_created, title, description }) {
+    const findInfo = informations.findIndex((info) => info.nasa_id === nasa_id);
+
+    const auxInformations = [...informations];
+
+    if (findInfo >= 0) {
+      auxInformations[findInfo].date_created = date_created;
+      auxInformations[findInfo].title = title;
+      auxInformations[findInfo].description = description;
+
+      setInformations(auxInformations);
+
+      localStorage.setItem('@NASA', JSON.stringify(informations));
+      onClose();
+
+      toast.success('Atualizado com sucesso!');
+
+      history.push('/');
+    }
+  }
+
+  return (
+    open && (
+      <Container>
+        <Card>
+          <Form onSubmit={handleEdit} initialData={initialValues}>
+            <p>ID: </p>
+            <Input type="text" name="nasa_id" value={data.nasa_id} disabled />
+            <p>Data Criação: </p>
+            <Input type="text" name="date_created" />
+            <p>Título: </p>
+            <Input type="text" name="title" />
+            <p>Descrição: </p>
+            <Input multiline name="description" />
+            <div>
+              <button type="submit">Atualizar</button>
+            </div>
+            <Icon onClick={onClose} />
+          </Form>
+        </Card>
+      </Container>
+    )
+  );
+}
+
+Modal.propTypes = {
+  onClose: PropTypes.func,
+  open: PropTypes.bool,
+  data: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]).isRequired,
+};
+Modal.defaultProps = {
+  onClose: true,
+  open: true,
+};
---
+++ src/components/Modal/styles.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+import { MdClose } from 'react-icons/md';
+
+export const Container = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 10;
+  background: rgba(0, 0, 0, 0.7);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Card = styled.div`
+  position: relative;
+  background: #fff;
+  border-radius: 8px;
+  width: 600px;
+  transition: 400ms;
+
+  padding: 20px;
+
+  p {
+    color: #000;
+    font-weight: bold;
+  }
+
+  input {
+    width: 300px;
+    padding: 10px;
+    border: 1px solid #000;
+    border-radius: 5px;
+    margin-bottom: 15px;
+  }
+
+  textarea {
+    width: 550px;
+    border: 1px solid #000;
+    border-radius: 5px;
+    padding: 10px;
+    height: 150px;
+    margin-bottom: 15px;
+
+    @media (max-width: 900px) {
+      width: 300px;
+    }
+  }
+
+  @media (max-width: 900px) {
+    width: 400px;
+  }
+
+  form > div {
+    display: flex;
+    justify-content: center;
+  }
+
+  form > div > button {
+    width: 120px;
+    height: 40px;
+    background: #0b3d91;
+    color: #fff;
+    border-radius: 4px;
+    border: 0;
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(90%);
+    }
+  }
+`;
+
+export const Icon = styled(MdClose)`
+  position: absolute;
+  height: 30px;
+  width: 30px;
+  color: #000000;
+  top: 5px;
+  right: 5px;
+  cursor: pointer;
+`;
